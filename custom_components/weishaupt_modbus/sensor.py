a77bdabf5f410071ad2e4cc7260e0185ce0e60a2
OStrama <66820413+OStrama@users.noreply.github.com>
2024-09-19 08:56:01 +0200

    Update sensor.py

--- custom_components/weishaupt_modbus/sensor.py
+++ custom_components/weishaupt_modbus/sensor.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import DiscoveryInfoType
@@ -18,8 +17,6 @@ async def async_setup_entry(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the sensor platform."""
-    host = config_entry.data[CONF_HOST]
-    port = config_entry.data[CONF_PORT]
     Entries = []
 
-    async_add_entities(BuildEntityList(Entries, host, port, MODBUS_SYS_ITEMS,TYPES.SENSOR), update_before_add=True)
+    async_add_entities(BuildEntityList(Entries, config_entry, MODBUS_SYS_ITEMS,TYPES.SENSOR), update_before_add=True)
