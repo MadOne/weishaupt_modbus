3ffb74312b0c9c5afb12f123debee8ce3898c619
OStrama <66820413+OStrama@users.noreply.github.com>
2024-09-19 07:45:51 +0200

    Update const.py
    Class structure changed

--- custom_components/weishaupt_modbus/const.py
+++ custom_components/weishaupt_modbus/const.py
@@ -1,8 +1,29 @@
-"""Constants."""
-
+from dataclasses import dataclass
 from datetime import timedelta
 
-DOMAIN = "weishaupt_modbus"
-SCAN_INTERVAL = timedelta(minutes=1)
-UNIQUE_ID = "unique_id"
-APPID = 100
+@dataclass(frozen=True)
+class MainConstants:
+    DOMAIN = "weishaupt_integ"
+    SCAN_INTERVAL = timedelta(minutes=1)
+    UNIQUE_ID = "unique_id"
+    APPID = 100
+
+CONST = MainConstants()
+
+@dataclass(frozen=True)
+class FormatConstants:
+    TEMPERATUR = "Temperatur"
+    PERCENTAGE = "Prozent"
+    NUMBER = "Nummer"
+    STATUS = "Status"
+
+FORMATS = FormatConstants()
+
+@dataclass(frozen=True)
+class TypeConstants:
+    SENSOR = "Sensor"
+    SELECT = "Select"
+    NUMBER = "Number"
+
+TYPES = TypeConstants()
+
