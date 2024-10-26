e546d9062ac0b185c8f2e43c99f4ff9b68f25d30
OStrama <66820413+OStrama@users.noreply.github.com>
2024-10-26 22:56:21 +0200

    Connection only opened once at startup

--- custom_components/weishaupt_modbus/__init__.py
+++ custom_components/weishaupt_modbus/__init__.py
@@ -22,6 +22,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # with your actual devices.
     # hass.data.setdefault(DOMAIN, {})[entry.entry_id] = hub.Hub(hass, entry.data["host"])
     mbapi = ModbusAPI(entry)
+    await mbapi.connect()
     entry.runtime_data = mbapi
 
     # This creates each HA object for each platform your device requires.
@@ -35,6 +36,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # This is called when an entry/configured device is to be removed. The class
     # needs to unload itself, and remove callbacks. See the classes for further
     # details
+    entry.runtime_data.close()
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[CONST.DOMAIN].pop(entry.entry_id)
