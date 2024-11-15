71fccfb6dceca432981cae5cb3f65117ba26da44
OStrama <66820413+OStrama@users.noreply.github.com>
2024-11-15 11:48:29 +0100

    add debug logging

--- custom_components/weishaupt_modbus/sensor.py
+++ custom_components/weishaupt_modbus/sensor.py
@@ -1,5 +1,7 @@
 """Setting uop my sensor entities."""
 
+import logging
+
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
@@ -11,6 +13,8 @@ from .hpconst import DEVICELISTS
 from .entities import build_entity_list
 from .coordinator import MyCoordinator
 
+logging.basicConfig()
+log = logging.getLogger(__name__)
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -35,6 +39,7 @@ async def async_setup_entry(
             entries, config_entry, device, TYPES.SENSOR_CALC, coordinator
         )
 
+        log.debug("Adding sensor entries to entity list ..")
         entries = await build_entity_list(
             entries, config_entry, device, TYPES.SENSOR, coordinator
         )
