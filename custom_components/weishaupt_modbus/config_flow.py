cc9cb07af1e00b337696975e992982c7cfc272e1
OStrama <66820413+OStrama@users.noreply.github.com>
2024-11-27 11:53:27 +0100

    Update config_flow.py

--- custom_components/weishaupt_modbus/config_flow.py
+++ custom_components/weishaupt_modbus/config_flow.py
@@ -81,7 +81,7 @@ async def validate_input(data: dict) -> dict[str, Any]:
 class ConfigFlow(config_entries.ConfigFlow, domain=CONST.DOMAIN):
     """Class config flow."""
 
-    VERSION = 6
+    VERSION = 5 #6
     # Pick one of the available connection classes in homeassistant/config_entries.py
     # This tells HA if it should be asking for updates, or it'll be notified of updates
     # automatically. This example uses PUSH, as the dummy hub will notify HA of
